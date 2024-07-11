ed7d0d0b24849cfda16a9171849a389ef8b20470
Mario <123633800+mescu@users.noreply.github.com>
2024-07-12 01:57:38 +0200

    Update m0Update.sh

--- m0Update.sh
+++ m0Update.sh
@@ -1,67 +1,38 @@
-!#/bin/bash
-#Created by mescu
-
-clear
-
+# !/bin/bash
+# Created by mescu
 echo "https://twitter.com/mescu"
-
 echo "https://github.com/mescu"
-
 echo "m0Update"
+echo
 
-function update() {
+update_system() {
+    echo
+    echo "This will update your system."
+    echo "Your system will be restarted after the update."
+    echo "Do you want to continue? [y/n]"
+    read -r choice
 
-	echo
+    if [[ "$choice" == "y" ]]; then
+        echo "Updating and restarting your system..."
+        sleep 1
 
-	echo "This will update your system"
-
-	sleep 1
-
-	echo
-
-	echo "When it finish your system will be restarted"
-
-	sleep 1
-
-	echo
-
-	echo "Do you want to continue? [y/n]"
-
-	read choice
-
-	if [ "$choice" = "y" ]; then
-
-		echo "Your system is going to be updated and restarted"
-
-		sleep 1
-
-		sudo apt update && sudo apt full-upgrade
-
-		sudo apt upgrade
-
-		reboot
-
-	fi
-
-	if [ "$choice" = "n" ]; then 
-
-		echo "Are you sure you want to cancel the process? [y/n]"
-
-		read choice2
-
-		if [ "$choice2" = "y" ]; then 
-
-			exit
-
-		fi
-
-		if [ "$choice2" = "n" ]; then 
-
-			update
-
-		fi
-fi
-
+        sudo apt update && sudo apt full-upgrade -y && sudo reboot
+    elif [[ "$choice" == "n" ]]; then
+        echo "Are you sure you want to cancel the update process? [y/n]"
+        read -r choice2
+        if [[ "$choice2" == "y" ]]; then
+            echo "Exiting update process."
+            exit
+        elif [[ "$choice2" == "n" ]]; then
+            update_system
+        else
+            echo "Invalid choice. Exiting."
+            exit 1
+        fi
+    else
+        echo "Invalid choice. Exiting."
+        exit 1
+    fi
 }
 
-update		
+update_system
